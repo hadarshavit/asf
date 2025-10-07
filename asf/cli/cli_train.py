80277159fc58282345aaacb4d42a4069527ab0cd
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2025-10-07 14:41:49 +0200

    CLI bugfix: selector no longer has performance metric as property

--- asf/cli/cli_train.py
+++ asf/cli/cli_train.py
@@ -63,13 +63,6 @@ def parser_function() -> argparse.ArgumentParser:
         help="Maximize the objective",
     )
     parser.add_argument(
-        "--performance-metric",
-        type=str,
-        default="",
-        required=False,
-        help="Performance metric to optimize",
-    )
-    parser.add_argument(
         "--feature-data",
         type=Path,
         required=True,
@@ -123,8 +116,6 @@ def build_cli_command(
         str(selector.budget),
         "--maximize",
         str(selector.maximize),
-        "--performance-metric",
-        str(selector.performance_metric),
         "--feature-data",
         str(feature_data),
         "--performance-data",
