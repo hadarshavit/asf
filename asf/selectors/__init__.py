58eac016cbca57ad806229d60a2efd30b5c4a899
Brian Schiller <brian.schiller@rwth-aachen.de>
2025-11-05 16:19:28 +0100

    Remaining path fixes

--- asf/selectors/__init__.py
+++ asf/selectors/__init__.py
@@ -1,6 +1,6 @@
 from asf.selectors.pairwise_classifier import PairwiseClassifier
 from asf.selectors.pairwise_regressor import PairwiseRegressor
-from asf.selectors.mutli_class import MultiClassClassifier
+from asf.selectors.multi_class import MultiClassClassifier
 from asf.selectors.performance_model import PerformanceModel
 from asf.selectors.simple_ranking import SimpleRanking
 from asf.selectors.joint_ranking import JointRanking
