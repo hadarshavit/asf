20d3bee4e3f5b275f35df21da8ce379fcaecaa7c
Brian Schiller <brian.schiller@rwth-aachen.de>
2025-11-14 15:41:27 +0100

    Added static 3S presolver

--- asf/presolving/__init__.py
+++ asf/presolving/__init__.py
@@ -1,9 +1,11 @@
 from asf.presolving.aspeed import Aspeed
 from asf.presolving.asap_v2 import ASAPv2
 from asf.presolving.presolver import AbstractPresolver
+from asf.presolving.static_3s import Static3S
 
 __all__ = [
     "Aspeed",
     "AbstractPresolver",
     "ASAPv2",
+    "Static3S",
 ]
